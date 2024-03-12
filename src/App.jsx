69a1e4bad59ec8aac4334bc84406797960e5e8bf
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-03-12 09:47:46 +0600

    display fake data by creating fake data manually when back end data is not ready to show.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Watch from './Components/Watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  const watches= [
+    {id:1, name: "Apple Watch", price: 234},
+    {id:2, name: "Orange Watch", price: 244},
+    {id:3, name: "Malta Watch", price: 134},
+    {id:4, name: "Grape Watch", price: 294},
+    {id:5, name: "Litchi Watch", price: 264},
+  ]
+  
 
   return (
     <>
       <h1>Vite + React</h1>
+
+      {
+        watches.map((watch)=><Watch key={watch.id} watch={watch}></Watch>)
+      }
+
     </>
   )
 }
---
+++ src/Components/Sunglass/Sunglass.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Sunglass = () => {
+    return (
+        <div>
+            <h2>Sunglass Name: {} </h2>
+        </div>
+    );
+};
+
+export default Sunglass;
