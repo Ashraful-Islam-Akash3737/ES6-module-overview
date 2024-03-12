0b006f44d3a77bc1ec03ddf8905eebbb6fa70763
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-03-12 10:11:46 +0600

    display fake data by creating watches.json file  manually when back end data is not ready to show.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-// import { useState } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react';
 import './App.css'
 import Watch from './Components/Watch/Watch'
+// import Watches from '../public/watches.json'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [watches, setWatches] = useState([]);
+  useEffect(()=>{
+    fetch('watches.json')
+    .then(res => res.json())
+    .then(data => setWatches(data))
+  },[])
 
-  const watches= [
-    {id:1, name: "Apple Watch", price: 234},
-    {id:2, name: "Orange Watch", price: 244},
-    {id:3, name: "Malta Watch", price: 134},
-    {id:4, name: "Grape Watch", price: 294},
-    {id:5, name: "Litchi Watch", price: 264},
-  ]
+  // const watches= [
+  //   {id:1, name: "Apple Watch", price: 234},
+  //   {id:2, name: "Orange Watch", price: 244},
+  //   {id:3, name: "Malta Watch", price: 134},
+  //   {id:4, name: "Grape Watch", price: 294},
+  //   {id:5, name: "Litchi Watch", price: 264},
+  // ]
   
 
   return (
     <>
       <h1>Vite + React</h1>
+      {/* <h3>{watches}</h3> */}
 
       {
         watches.map((watch)=><Watch key={watch.id} watch={watch}></Watch>)
